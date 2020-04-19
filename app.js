b2653ded290f7c56d81b1afbcf273a2428b1411a
imadharilla <imadharilla@gmail.com>
2020-04-19 19:21:28 +0000

    ehehehe

---
+++ app.js
@@ -0,0 +1,64 @@
+
+const path = require("path");
+const express = require('express');
+
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const eventsRoutes = require('./routes/events');
+const userRoutes = require('./routes/users');
+const attendeeRoutes = require('./routes/attendee');
+const app = express();
+
+
+
+
+
+
+app.use(express.static(path.join(__dirname,'/public')));
+
+app.get((req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+
+/*
+mongoose.connect('mongodb://localhost/eventsdb',{ useNewUrlParser: true , useUnifiedTopology: true  }  )
+.then(() => {
+  console.log('Connected to database !');
+})
+.catch(() => {
+  console.log('Connection failed!');
+});
+
+*/
+
+mongoose.connect('mongodb+srv://Mata:4RruaXfcdPVZMDX@cluster0-vzdpf.mongodb.net/test?retryWrites=true&w=majority' )
+.then(() => {
+  console.log('Connected to database !');
+})
+.catch(() => {
+  console.log('Connection failed!');
+});
+
+
+app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname + "backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader("Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader("Access-Control-Allow-Methods",
+  "GET, POST, PATCH, DELETE, PUT ,OPTIONS");
+  next();
+});
+
+
+app.use("/api/events", eventsRoutes);
+
+app.use("/api/user", userRoutes);
+
+app.use("/api/attendee", attendeeRoutes);
+
+module.exports = app;
---
+++ models/event.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema({
+
+  title: {type: String, required:true},
+  imagePath: {type:String, required: true },
+  description : {type:String, required: false},
+  startDate : {type:Date , required:true},
+  endDate : {type:Date , required:true},
+  location : {type: String, required:true},
+  creator : { type: mongoose.Schema.Types.ObjectId, ref:'User' ,required: true},
+  attendeeList: {type: Array, required: false,}
+});
+
+module.exports = mongoose.model('Event', eventSchema);
+

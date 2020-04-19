10780d12025938a6e862632c14677cf89772f6ba
imadharilla <imadharilla@gmail.com>
2020-04-19 19:50:42 +0000

    ehehehe

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ const app = express();
 
 
 
-app.use(express.static(path.join(__dirname,'/public')));
-
-app.get((req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-
 
 /*
 mongoose.connect('mongodb://localhost/eventsdb',{ useNewUrlParser: true , useUnifiedTopology: true  }  )
